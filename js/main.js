e3cbd696ddf6161f7e59d5cebbd4d5e57515b8be
0x004d4m <0x004d4m@gmail.com>
2022-05-31 14:43:51 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -95,14 +95,14 @@
 // Scroll to specific values
 // scrollTo is the same
 window.scroll({
-    top: 2500, 
+    top: 0, 
     left: 0, 
     behavior: 'smooth'
   });
   
   // Scroll certain amounts from current position 
   window.scrollBy({ 
-    top: 100, // could be negative value
+    top: 0, // could be negative value
     left: 0, 
     behavior: 'smooth' 
   });
